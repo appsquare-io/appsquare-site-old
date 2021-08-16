d5c17f1d31eda5f2f14d5909274b3b45b49cb799
Louis Smit <louissm.it@icloud.com>
2021-08-17 01:48:27 +0200

    feat: Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from 'react'
+import { MenuAlt4Icon } from '@heroicons/react/outline'
+import { AnimatePresence } from 'framer-motion'
+import { Column } from 'components'
+import Menu from 'components/Menu'
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const closeMenu = useCallback((event) => {
+    if (event.keyCode === 27) setShowMenu(false)
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('keydown', closeMenu, false)
+
+    return () => {
+      document.removeEventListener('keydown', closeMenu, false)
+    }
+  }, [])
+  return (
+    <>
+      <AnimatePresence>
+        {showMenu && <Menu onClose={() => setShowMenu(false)} />}
+      </AnimatePresence>
+      <Column>
+        <div className="flex items-center justify-between w-full my-14">
+          <img src="/A.png" alt="Appsquare logo" className="w-auto h-10" />
+          <button onClick={() => setShowMenu(true)}>
+            <MenuAlt4Icon className="w-auto h-8 cursor-pointer hover:text-gray-600" />
+          </button>
+        </div>
+      </Column>
+    </>
+  )
+}
+
+export default Navbar
