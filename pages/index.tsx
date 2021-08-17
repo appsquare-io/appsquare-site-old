df7f90ace193045bc3a53c4383367efa09c7da42
Louis Smit <louissm.it@icloud.com>
2021-08-17 11:24:54 +0200

    feat: Introduction links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import NextLink from 'next/link'
 import { Column } from 'components'
 import Head from 'components/Head'
 import Navbar from 'components/Navbar'
@@ -9,19 +10,18 @@ const Home = () => {
       <Navbar />
       <Head />
       <main>
-        <Column size="small" className="mt-0 sm:mt-20 md:mt-32">
+        <Column size="small" className="mt-0 sm:mt-20 md:mt-28">
           <h1 className="text-3xl font-extrabold text-transparent sm:text-4xl md:text-6xl bg-clip-text bg-gradient-to-br from-red-400 to-pink-600">
             Hands-on digital agency for delightful apps, websites, and ideas.
           </h1>
 
           <p className="mt-5 text-2xl font-semibold text-gray-800 sm:text-3xl">
-            Appsquare <span className="line-through">builds</span> crafts
-            digital experiences for{' '}
-            <a href="" className="font-bold text-pink-600">
-              awesome companies
-            </a>{' '}
-            around the world. We strive for efficiency, using the best tools for
-            the job, and getting straight to the point.
+            Appsquare <span className="line-through">builds</span> crafts{' '}
+            <Link href="/projects">digital experiences</Link> for{' '}
+            <Link href="/clients">awesome companies</Link> around the world. We
+            strive for efficiency, using the best{' '}
+            <Link href="/tools">tools</Link> for the job, and getting straight
+            to the point.
             <br />
           </p>
 
@@ -38,5 +38,11 @@ const Home = () => {
     </>
   )
 }
+
+const Link = ({ children, href }) => (
+  <NextLink href={href} passHref>
+    <a className="font-bold text-pink-600">{children}</a>
+  </NextLink>
+)
 
 export default Home
