00784bc29ecebd9367a857def559af58087e1e76
Louis Smit <louissm.it@icloud.com>
2021-08-19 00:48:09 +0200

    refactor: PageHeader scaling

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -7,7 +7,7 @@ const PageHeader = ({ children, as }: PageHeaderProps) => {
   const Tag = (as || 'h1') as keyof JSX.IntrinsicElements
 
   return (
-    <Tag className="text-3xl font-extrabold text-transparent sm:text-4xl md:text-6xl bg-clip-text bg-gradient-to-br from-red-400 to-pink-600">
+    <Tag className="text-3xl font-extrabold text-transparent sm:text-4xl md:text-5xl lg:text-6xl 2xl:text-5xl bg-clip-text bg-gradient-to-br from-red-400 to-pink-600">
       {children}
     </Tag>
   )
